a096eb8da946bef0b13f4fc2eeb400076bd21366
nagarazb1992 <115771512+nagarazb1992@users.noreply.github.com>
2026-01-24 16:16:14 +0530

    update ssl

--- src/config/database.ts
+++ src/config/database.ts
@@ -12,6 +12,9 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: false, // Set to console.log to see SQL queries
+    dialectOptions: {
+      ssl: false, // IMPORTANT (matches: "no encryption")
+    },
   }
 );
 
