f3cc93cbff1af6ee26c92c4da09d1c511f3d2e53
Nag Raz <nagarzb1992@gmail.com>
2026-01-26 16:02:29 +0530

    updated payment gateway

--- src/utils/dodoClient.ts
+++ src/utils/dodoClient.ts
@@ -2,5 +2,5 @@ import DodoPayments from "dodopayments";
 
 export const dodoClient = new DodoPayments({
   bearerToken: process.env.DODO_PAYMENTS_API_KEY!,
-  environment: "test_mode", // change to "live_mode" in prod
+  environment: "live_mode", // change to "live_mode" in prod test_mode
 });
